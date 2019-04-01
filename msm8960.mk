92a008b7d97b5624f52a6deb61aa9c18aef79cb9
TARKZiM <tom8476oo@gmail.com>
2019-04-02 00:14:08 +0800

    msm8960-common: switch back to 1.0 power hal

--- msm8960.mk
+++ msm8960.mk
@@ -110,7 +110,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.0-service-qti
 
 # Thermal
 PRODUCT_PACKAGES += \
