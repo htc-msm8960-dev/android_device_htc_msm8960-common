cb4bf03136b44dc0ced20c33ad47587ae0298d01
TARKZiM <tom8476oo@gmail.com>
2019-04-04 13:48:10 +0800

    Revert "msm8960-common: properly include system_prop.mk"
    This reverts commit c4dcdd5f2eaf8eea5b9334b4d387dce24ada50de.

--- msm8960.mk
+++ msm8960.mk
@@ -31,7 +31,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
 # System properties
-include $(LOCAL_PATH)/system_prop.mk
+-include $(LOCAL_PATH)/system_prop.mk
 
 # Audio
 PRODUCT_PACKAGES += \
