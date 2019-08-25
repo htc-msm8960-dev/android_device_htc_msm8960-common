1d4fc3b146168b62f79702e1ba43cf7c7e3f3464
TARKZiM <tom8476oo@gmail.com>
2019-08-25 17:27:01 +0800

    msm8960-common: Add mediaserver to SDK Override list

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -82,7 +82,8 @@ TARGET_PROCESS_SDK_VERSION_OVERRIDE := \
     /system/vendor/bin/hw/android.hardware.sensors@1.0-service.htc8960=21 \
     /system/vendor/bin/hw/android.hardware.media.omx@1.0-service=21 \
     /system/vendor/bin/hw/rild=27 \
-    /system/bin/audioserver=21
+    /system/bin/audioserver=21 \
+    /system/bin/mediaserver=21
 
 # Charge mode
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/htc_lpm/lpm_mode
