71f5d1667575672214ee883c50bd20c2a5e1fcf9
TARKZiM <tom8476oo@gmail.com>
2019-09-18 13:56:15 +0800

    msm8960-common: Update config.fs for extra root folders

--- config.fs
+++ config.fs
@@ -9,3 +9,39 @@ value:2951
 
 [AID_VENDOR_RFS_SHARED]
 value:2952
+
+[firmware/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[vendor/firmware_mnt/image/*]
+mode: 0771
+user: AID_ROOT
+group: AID_SYSTEM
+caps: 0
+
+[firmware/image/*]
+mode: 0771
+user: AID_ROOT
+group: AID_SYSTEM
+caps: 0
+
+[devlog/]
+mode: 0771
+user: AID_ROOT
+group: AID_SYSTEM
+caps: 0
+
+[ramdump/]
+mode: 0771
+user: AID_ROOT
+group: AID_SYSTEM
+caps: 0
+
+[persist/]
+mode: 0771
+user: AID_ROOT
+group: AID_SYSTEM
+caps: 0
