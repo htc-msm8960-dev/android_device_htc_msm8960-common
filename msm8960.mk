937fffd09019789f46075ca2f47b77b3c3dd6883
TARKZiM <tom8476oo@gmail.com>
2019-10-02 00:02:18 +0800

    msm8960-common: Re-enable amplifier hal

--- msm8960.mk
+++ msm8960.mk
@@ -49,6 +49,7 @@ PRODUCT_COPY_FILES += \
 # Audio
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
+    audio_amplifier.msm8960 \
     audio.primary.msm8960 \
     audio.r_submix.default \
     audio.usb.default \
