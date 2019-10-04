be036ceb97493bb780283b705c1e3b94631b22b7
Bruno Martins <bgcngm@gmail.com>
2019-10-04 23:20:42 +0100

    msm8960-common: Switch to legacy Wi-Fi service
    Change-Id: I690bc80349e47907a29a89b8a6f96659f1c61aba

--- msm8960.mk
+++ msm8960.mk
@@ -237,7 +237,7 @@ PRODUCT_PACKAGES += \
 
 # WiFi HIDL interfaces
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service
+    android.hardware.wifi@1.0-service.legacy
 
 # WiFi
 PRODUCT_PACKAGES += \
