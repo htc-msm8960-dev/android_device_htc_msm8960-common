983baa53e8299825e61eb35145040e7f0247d17c
TARKZiM <tom8476oo@gmail.com>
2019-12-16 14:30:36 +0800

    msm8960-common: Remove DRM ClearKey 1.2 HAL
    * We don't support it.

--- msm8960.mk
+++ msm8960.mk
@@ -116,8 +116,7 @@ PRODUCT_PACKAGES += \
 # DRM HIDL interfaces
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
-    android.hardware.drm@1.2-service.clearkey
+    android.hardware.drm@1.0-service
 
 # Encryption
 PRODUCT_PACKAGES += \
