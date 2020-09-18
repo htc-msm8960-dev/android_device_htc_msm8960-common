242ab28150c8e7690c405f85469db91a1e9c54a9
LuK1337 <priv.luk@gmail.com>
2020-09-18 14:36:05 +0200

    msm8960-common: Build libhidltransport/libhwbinder
    * Needed for old blobs
    
    Change-Id: I43fc0e46547dfc126a4b2ac9dcc82c7c4f2ee172

--- msm8960.mk
+++ msm8960.mk
@@ -135,7 +135,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0 \
-    android.hidl.manager@1.0-java
+    android.hidl.manager@1.0-java \
+    libhidltransport \
+    libhidltransport.vendor \
+    libhwbinder \
+    libhwbinder.vendor
 
 # Keymaster
 PRODUCT_PACKAGES += \
