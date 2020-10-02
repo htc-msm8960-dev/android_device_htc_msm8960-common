1e7d2c4f8f48ddb47105bdaf2fa8839a95059e30
Jaroslav Zviezda <acroreiser@gmail.com>
2020-10-02 21:31:53 +0300

    msm8960-common: build software Gatekeeper

--- msm8960.mk
+++ msm8960.mk
@@ -151,7 +151,8 @@ PRODUCT_PACKAGES += \
 # Keymaster
 PRODUCT_PACKAGES += \
     keystore.msm8960 \
-    android.hardware.keymaster@3.0-impl
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.gatekeeper@1.0-service.software
 
 # IPv6 tethering
 PRODUCT_PACKAGES += \
