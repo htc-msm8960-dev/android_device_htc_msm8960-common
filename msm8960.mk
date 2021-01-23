ffaaa459a5a3aaaf16b973c542bcb62757718193
z3DD3r <2339201+z3DD3r@users.noreply.github.com>
2021-01-23 19:25:55 +0300

    msm8960-common: fastcharge: Build FastCharge HAL

--- msm8960.mk
+++ msm8960.mk
@@ -125,6 +125,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service
 
+# FastCharge
+PRODUCT_PACKAGES += \
+    vendor.lineage.fastcharge@1.0-service.htc8960
+
 # GNSS HAL
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl.htc8960 \
