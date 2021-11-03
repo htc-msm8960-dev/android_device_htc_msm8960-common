3c3bffd12698a7d6aab8fb5ad59cb91257880cf7
TARKZiM <tom8476oo@gmail.com>
2021-11-04 02:35:38 +0800

    msm8960-common: Remove duplicated properties

--- msm8960.mk
+++ msm8960.mk
@@ -19,8 +19,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/go_defaults.mk)
 # Dalvik vm configs
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapstartsize=16m \
-    dalvik.vm.heapgrowthlimit=192m \
-    dalvik.vm.heapsize=512m \
     dalvik.vm.heaptargetutilization=0.75 \
     dalvik.vm.heapminfree=2m \
     dalvik.vm.heapmaxfree=8m
